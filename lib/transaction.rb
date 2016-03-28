3a5f563d62313171a2de5aec4a7a5dcba453d9f1
Ankhbayar Baasandorj <antony.baasan@gmail.com>
2016-03-27 22:28:27 -0600

    basic Transaction class created

---
+++ lib/transaction.rb
@@ -0,0 +1,35 @@
+require_relative "errors"
+
+class Transaction
+    attr_reader :id
+    attr_accessor :customer, :product, :id
+
+    @@id = 1
+    @@all_transactions = []
+
+    def initialize(customer, product)
+        @customer = customer
+        @product = product
+        
+        @id = @@id
+        @@id += 1
+
+        push_to_all_list self
+        
+        update_product_stock
+    end
+    
+    def self.all
+        @@all_transactions
+    end
+    
+    private 
+    def push_to_all_list(transaction)
+        @@all_transactions.push(transaction)    
+    end
+    
+    def update_product_stock
+        @product.stock -= 1
+    end
+
+end
