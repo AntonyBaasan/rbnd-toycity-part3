6dc032803a03f8b7d5c4c50389637a5fe0155d7c
Antony Baasandorj <antony.baasan@gmail.com>
2016-03-27 20:08:42 -0600

    add all static method

--- lib/product.rb
+++ lib/product.rb
@@ -1,9 +1,17 @@
 class Product
     attr_accessor :title, :price, :stock
+    
+    @@all_products = []
 
     def initialize(params={})
         @title = params[:title]
         @price = params[:price]
         @stock = params[:stock]
+        
+        @@all_products.push(self)
+    end
+    
+    def self.all
+        @@all_products
     end
 end
